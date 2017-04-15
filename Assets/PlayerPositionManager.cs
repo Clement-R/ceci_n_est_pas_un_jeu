1bf31314db328a94b33b1b9b5ebd0efd261d74e6
Clement-R <rondeau.clement@gmail.com>
2017-04-15 22:06:06 +0200

    Add first assets and player movement with position detection on the map

---
+++ Assets/PlayerPositionManager.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPositionManager : MonoBehaviour {
+    private Vector2 position;
+    
+    void Update() {
+        Debug.Log(Mathf.RoundToInt(Mathf.Abs(transform.position.x)) + " : " + Mathf.RoundToInt(Mathf.Abs(transform.position.z)));
+    }
+}
