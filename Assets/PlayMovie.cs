5d31d2a446366b4c00e44693582bd1f7ee816279
Clement-R <rondeau.clement@gmail.com>
2017-04-16 12:57:37 +0200

    Add demo scene, MP4 test and invisible walls

---
+++ Assets/PlayMovie.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayMovie : MonoBehaviour {
+	void Start () {
+        MovieTexture tex = GetComponent<Renderer>().material.mainTexture as MovieTexture;
+        tex.loop = true;
+        tex.Play();
+    }
+}
